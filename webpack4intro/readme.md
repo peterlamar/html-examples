036260fb4e61576ba3b3e5221feca66f800677ed
peterlamar <peter.r.lamar@gmail.com>
2019-03-13 16:52:19 -0400

    Web pack 4 intro

---
+++ webpack4intro/readme.md
@@ -0,0 +1,6 @@
+# Webpack intro
+
+
+## Reference
+
+[Webpack 4 intro](https://hackernoon.com/a-tale-of-webpack-4-and-how-to-finally-configure-it-in-the-right-way-4e94c8e7e5c1)
---
+++ webpack4intro/webpack.config.js
@@ -0,0 +1,49 @@
+// webpack v4
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    target: 'node', // update from 23.12.2018
+    externals: [nodeExternals()], // update from 23.12.2018
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  {
+                    loader: MiniCssExtractPlugin.loader
+                  },
+                  "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: "style.css"
+        }),
+        new HtmlWebpackPlugin({
+          inject: false,
+          hash: true,
+          template: './src/index.html',
+          filename: 'index.html'
+        })
+    ],
+};
