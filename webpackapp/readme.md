7805274f3faaad71fd17f0e159a59b8207af619a
peterlamar <peter.r.lamar@gmail.com>
2019-03-13 13:13:30 -0400

    Webpack tutorial basics

---
+++ webpackapp/readme.md
@@ -0,0 +1,28 @@
+# Webpack Starter App
+
+Webpack dev environment
+
+Trigger webpack manually
+
+```
+npx webpack app.js -o bundle.js
+```
+
+## Install Dependencies
+```bash
+npm install 
+```
+
+## Run Dev Server
+```bash
+npm start
+```
+
+## Build To Dist Folder
+```bash
+npm run build
+```
+
+## Reference
+
+[Traversy Media Webpack Tutorial](https://www.youtube.com/watch?v=lziuNMk_8eQ)
---
+++ webpackapp/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+
+                }
+            }
+        ]
+    }
+}
